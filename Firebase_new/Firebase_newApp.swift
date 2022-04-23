c40c27c262b3f446a8421352399733c1033fed18
Klevia Ulqinaku <klevia.96@gmail.com>
2022-04-23 18:30:19 +0200

    Initial Commit

---
+++ Firebase_new/Firebase_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_newApp.swift
+//  Firebase_new
+//
+//  Created by Klevia Ulqinaku on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
